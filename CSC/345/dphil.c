178e73a21560d4e30f4d058597cb3bfc12cfd7ad
Zefaxet <getmemedkid@dicksinhisan.us>
2019-01-28 17:28:02 -0600

    work

--- CSC/345/dphil.c
+++ CSC/345/dphil.c
@@ -3,11 +3,11 @@
 #include <pthread.h>
 #include <string.h>
 
-// #define nPhilosophers 20
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-void* eat(void* nHunger);
-int count = 0;
+#define DEBUG 0
+pthread_mutex_t * chopsticks;
+pthread_mutex_t init_mutex = PTHREAD_MUTEX_INITIALIZER;
+void* start(void* philosopher);
+int nHunger;
 
 int main( int argc, char** argv)
 {
@@ -16,7 +16,7 @@ int main( int argc, char** argv)
 	{
 		
 		int nPhilosophers = strtol(argv[2], (char**) NULL, 10);
-		int nHunger = strtol(argv[1], (char**) NULL, 10);
+		nHunger = strtol(argv[1], (char**) NULL, 10);
 		
 		printf("Number of philosophers: %d\n", nPhilosophers);
 		if (nPhilosophers < 2)
@@ -27,14 +27,28 @@ int main( int argc, char** argv)
 		}
 		printf("Each philosopher will eat %d times.\n", nHunger);
 		
+		pthread_mutex_t mutexes[nPhilosophers];
+		for (int i = 0; i < nPhilosophers; i++)
+		{
+			puts("make mutex");
+			pthread_mutex_init(&mutexes[i], NULL);
+			
+		}
+		
+		chopsticks = mutexes;
+		
 		pthread_t tThreads[nPhilosophers];
 		
 		int i, j;
 		for(i=0; i < nPhilosophers; i++)
 		{
-			
-			printf("Create thread %d\n", i);
-			pthread_create( &tThreads[i], NULL, &eat, (void*) &nHunger);
+			int * philosopher = &i;
+			if (DEBUG)
+			{
+				printf("Create thread %d\n", i);
+			}
+			pthread_mutex_lock( &init_mutex );
+			pthread_create( &tThreads[i], NULL, &start, (void*) philosopher);
 			
 		}
 		
@@ -49,7 +63,7 @@ int main( int argc, char** argv)
 				goto leave;
 				
 			}
-			else
+			else if (DEBUG)
 			{
 				
 				printf("Join thread success: %d.\n", j);
@@ -65,11 +79,19 @@ int main( int argc, char** argv)
 	
 }
 
-void* eat(void* nHunger)
+void * start(void * i)
 {	
-	// printf("\tHunger %d\n", *(int*) nHunger);
-	pthread_mutex_lock( &mutex );
-	count++;
-	printf("\tCount %d\n", count);
-	pthread_mutex_unlock( &mutex );
+	int philosopher = * (int *) i;
+	int hunger = nHunger;
+	printf("Philosopher %d has joined the table.\n", philosopher);
+	pthread_mutex_unlock( &init_mutex );
+	while(hunger > 0)
+	{
+		printf("Philosopher %d is thinking.\n", philosopher);
+		pthread_mutex_lock( (chopsticks + sizeof(pthread_mutex_t) * philosopher) );
+		pthread_mutex_lock( (chopsticks + sizeof(pthread_mutex_t) * (philosopher + 1)) );
+		printf("Philosopher %d has eaten. Hunger is now %d.\n", philosopher, --hunger);
+		pthread_mutex_unlock( (chopsticks + sizeof(pthread_mutex_t) * philosopher) );
+		pthread_mutex_unlock( (chopsticks + sizeof(pthread_mutex_t) * (philosopher + 1)) );
+	}
 }
