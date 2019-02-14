3687fb68cdd67cf42d7eeda7341e945a58ae8032
zefaxet <zefaxet@gmail.com>
2019-02-14 14:28:26 -0600

    parse()

--- CSC/345/tsh/tsh.c
+++ CSC/345/tsh/tsh.c
@@ -48,9 +48,171 @@ void lambda(char * key, char * value, void * obj)
 	printf("%s: %s\n", key, value);
 }
 
-void parse(char * args, int stdin_fd)
+void parse(char * in, int stdin_fd)
 {
 	
+	char * outref;
+	char * inref;
+	char * fileref;
+
+	//REDIRECTION
+	if((outref = strstr(in, ">")))
+	{
+		
+		fileref = strtok(outref + 1, DELIM);
+		outfd = open(fileref, O_TRUNC | O_WRONLY | O_CREAT); 
+		if (dup2(outfd, 1) < 0)
+		{
+			perror("Failed to duplicate stdout.");
+			return;
+		}
+
+		*outref = (char) NULL;
+
+	}
+	if((inref = strstr(in, "<")))
+	{
+
+		fileref = strtok(inref + 1, DELIM);
+		infd = open(fileref, O_RDONLY);
+		if (errno == ENOENT)
+		{
+			perror(strerror(errno));
+			return;
+		}
+		if (dup2(infd, 0) < 0)
+		{
+			perror("Failed to duplicate stdin.");
+			return;
+		}
+
+		*inref = (char) NULL;
+
+	}
+	//REDIRECTION~		
+
+	token = strtok(in, DELIM);
+	trim_trailing_whitespace(token);
+	
+	if (token != NULL)
+	{
+		if(!strcmp(token, EXIT))
+		{
+			
+			token = strtok(NULL, DELIM);
+			if (token == NULL)
+			{
+				printf("Exiting with status code 0.\n");
+				exit(0);
+			}
+			else if (isdigit(*token))
+			{
+				int code = *token - '0';
+				printf("Exiting with status code %d\n",code);
+				exit(code);
+			}
+			else
+			{
+				printf("Invalid exit code '%s'. Exiting with -1.\n", token);
+				exit(-1);
+			}
+		}
+		else if(!strcmp(token, PWD))
+		{
+			printf("%s\n",cwd);
+			return;
+		}
+		else if(!strcmp(token, CD))
+		{
+			token = strtok(NULL, DELIM);
+			chdir(token);
+			return;
+		}
+		else if(!strcmp(token, SET))
+		{
+			token = strtok(NULL, DELIM);
+			if (token)
+			{
+				char * identifier = token;
+				token = strtok(NULL, "\n");
+				if (token)
+				{
+					char * value = token;
+					sm_put(symbol_table, identifier, value);
+					goto set_success;
+				}
+				else
+				{
+					goto set_error;
+				}
+			}
+			else
+			{
+				goto set_error;
+			}
+			
+			set_error:
+			puts("Invalid usage: set <name> <value>");
+			set_success:
+			return;
+		}
+		else if(!strcmp(token, LIST))
+		{
+			
+			sm_enum(symbol_table, (sm_enum_func) lambda, NULL); 
+		}
+		else
+		{
+
+			int i = 0;
+
+			do
+			{
+				
+				args[i++] = token;	
+
+			}
+			while( (token = strtok( NULL, DELIM )) != NULL);
+			
+			if ((child = fork()) == 0)
+			{
+				
+				if(execvp(args[0],args) == -1)
+				{
+					printf("You tryna run this '%s' trash, fam? Aight bet.\n",args[0]);
+					exit(-1);
+				}
+				
+
+			}
+			else
+			{
+				
+				if (child == (pid_t)(-1))
+				{
+					puts("Fork failed.");
+				}
+				else
+				{
+					c == wait(&cstatus);
+				}
+			}
+
+		}
+
+	}
+
+	if (outfd != 1)
+	{
+		fflush(stdout);
+		close(outfd);
+	}
+	if (infd != 0)
+	{
+		fflush(stdin);
+		close(infd);
+	}
+
 }
 
 int main()
@@ -79,167 +241,7 @@ int main()
 	
 		fgets(in, sizeof(in), stdin);
 		
-		char * outref;
-		char * inref;
-		char * fileref;
-
-		//REDIRECTION
-		if((outref = strstr(in, ">")))
-		{
-			
-			fileref = strtok(outref + 1, DELIM);
-			outfd = open(fileref, O_TRUNC | O_WRONLY | O_CREAT); 
-			if (dup2(outfd, 1) < 0)
-			{
-				perror("Failed to duplicate stdout.");
-				continue;
-			}
-
-			*outref = (char) NULL;
-
-		}
-		if((inref = strstr(in, "<")))
-		{
-
-			fileref = strtok(inref + 1, DELIM);
-			infd = open(fileref, O_RDONLY);
-			if (errno == ENOENT)
-			{
-				perror(strerror(errno));
-				continue;
-			}
-			if (dup2(infd, 0) < 0)
-			{
-				perror("Failed to duplicate stdin.");
-				continue;
-			}
-
-			*inref = (char) NULL;
-
-		}
-		//REDIRECTION~		
-
-		token = strtok(in, DELIM);
-		trim_trailing_whitespace(token);
-		
-		if (token != NULL)
-		{
-			if(!strcmp(token, EXIT))
-			{
-				
-				token = strtok(NULL, DELIM);
-				if (token == NULL)
-				{
-					printf("Exiting with status code 0.\n");
-					exit(0);
-				}
-				else if (isdigit(*token))
-				{
-					int code = *token - '0';
-					printf("Exiting with status code %d\n",code);
-					exit(code);
-				}
-				else
-				{
-					printf("Invalid exit code '%s'. Exiting with -1.\n", token);
-					exit(-1);
-				}
-			}
-			else if(!strcmp(token, PWD))
-			{
-				printf("%s\n",cwd);
-				continue;
-			}
-			else if(!strcmp(token, CD))
-			{
-				token = strtok(NULL, DELIM);
-				chdir(token);
-				continue;
-			}
-			else if(!strcmp(token, SET))
-			{
-				token = strtok(NULL, DELIM);
-				if (token)
-				{
-					char * identifier = token;
-					token = strtok(NULL, "\n");
-					if (token)
-					{
-						char * value = token;
-						sm_put(symbol_table, identifier, value);
-						goto set_success;
-					}
-					else
-					{
-						goto set_error;
-					}
-				}
-				else
-				{
-					goto set_error;
-				}
-				
-				set_error:
-				puts("Invalid usage: set <name> <value>");
-				set_success:
-				continue;
-			}
-			else if(!strcmp(token, LIST))
-			{
-				
-				sm_enum(symbol_table, (sm_enum_func) lambda, NULL); 
-			}
-			else
-			{
-
-				int i = 0;
-
-				do
-				{
-					
-					args[i++] = token;	
-
-				}
-				while( (token = strtok( NULL, DELIM )) != NULL);
-				
-				if ((child = fork()) == 0)
-				{
-					
-					if(execvp(args[0],args) == -1)
-					{
-						printf("You tryna run this '%s' trash, fam? Aight bet.\n",args[0]);
-						exit(-1);
-					}
-					
-
-				}
-				else
-				{
-					
-					if (child == (pid_t)(-1))
-					{
-						puts("Fork failed.");
-					}
-					else
-					{
-						c == wait(&cstatus);
-					}
-				}
-
-			}
-
-		}
-
-		if (outfd != 1)
-		{
-			fflush(stdout);
-			close(outfd);
-		}
-		if (infd != 0)
-		{
-			fflush(stdin);
-			close(infd);
-		}
+		parse(in, stdin_fd);
 
 	}
 }
