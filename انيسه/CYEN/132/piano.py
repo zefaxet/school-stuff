1ae2f9e13bf62efd623545ce2e46eaff4a30c78c
zefaxet <darkraisdeoxys@gmail.com>
2019-05-05 22:36:47 -0500

    idk

--- انيسه/CYEN/132/piano.py
+++ انيسه/CYEN/132/piano.py
@@ -16,79 +16,79 @@ MIXER_BUFF = 1024
 
 # the note generator class
 class Note(pygame.mixer.Sound):
-    
-    # note that volume ranges from 0.0 to 1.0
-    def __init__(self, frequency, volume):
-        self.frequency = frequency
-        # initialize the note using an array of samples
-        pygame.mixer.Sound.__init__(self,\
-            buffer=self.build_samples())
-        self.set_volume(volume)
+	
+	# note that volume ranges from 0.0 to 1.0
+	def __init__(self, frequency, volume):
+		self.frequency = frequency
+		# initialize the note using an array of samples
+		pygame.mixer.Sound.__init__(self,\
+			buffer=self.build_samples())
+		self.set_volume(volume)
 
-    # builds an array of samples for the current note
-    def build_samples(self):
-        # calculate the period and amplitude of the note's wave
-        period = int(round(MIXER_FREQ / self.frequency))
-        amplitude = 2 ** (abs(MIXER_SIZE) - 1) - 1
-        # initialize the note's samples (using an array of
-        # signed 16-bit "shorts")
-        samples = array("h", [0] * period)
-        
-        # generate the note's samples
-        for t in range(period):
-            if (t < period / 2):
-                samples[t] = amplitude
-            else:
-                samples[t] = -amplitude
-                
-        return samples
+	# builds an array of samples for the current note
+	def build_samples(self):
+		# calculate the period and amplitude of the note's wave
+		period = int(round(MIXER_FREQ / self.frequency))
+		amplitude = 2 ** (abs(MIXER_SIZE) - 1) - 1
+		# initialize the note's samples (using an array of
+		# signed 16-bit "shorts")
+		samples = array("h", [0] * period)
+		
+		# generate the note's samples
+		for t in range(period):
+			if (t < period / 2):
+				samples[t] = amplitude
+			else:
+				samples[t] = -amplitude
+				
+		return samples
 
 # waits until a note is pressed
 def wait_for_note_start():
-    while (True):
-        # first, check for notes
-        for key in range(len(keys)):
-            if (GPIO.input(keys[key])):
-                return key
-        # next, check for the play button
-        if (GPIO.input(play)):
-            # debounce the switch
-            while (GPIO.input(play)):
-                sleep(0.01)
-            return "play"
-        # finally, check for the record button
-        if (GPIO.input(record)):
-            # debounce the switch
-            while (GPIO.input(record)):
-                sleep(0.01)
-            return "record"
-        sleep(0.01)
+	while (True):
+		# first, check for notes
+		for key in range(len(keys)):
+			if (GPIO.input(keys[key])):
+				return key
+		# next, check for the play button
+		if (GPIO.input(play)):
+			# debounce the switch
+			while (GPIO.input(play)):
+				sleep(0.01)
+			return "play"
+		# finally, check for the record button
+		if (GPIO.input(record)):
+			# debounce the switch
+			while (GPIO.input(record)):
+				sleep(0.01)
+			return "record"
+		sleep(0.01)
 
 # waits until a note is released
 def wait_for_note_stop(key):
-    while (GPIO.input(key)):
-        sleep(0.1)
+	while (GPIO.input(key)):
+		sleep(0.1)
 
 # plays a recorded song
 def play_song():
-    # each element in the song list is a list composed of two
-    # parts: a note (or silence) and a duration
-    for part in song:
-        note, duration = part
-        # if it's a silence, delay for its duration
-        if (note == "SILENCE"):
-            sleep(duration)
-        # otherwise, play the note for its duration
-        else:
-            notes[note].play(-1)
-            sleep(duration)
-            notes[note].stop()
+	# each element in the song list is a list composed of two
+	# parts: a note (or silence) and a duration
+	for part in song:
+		note, duration = part
+		# if it's a silence, delay for its duration
+		if (note == "SILENCE"):
+			sleep(duration)
+		# otherwise, play the note for its duration
+		else:
+			notes[note].play(-1)
+			sleep(duration)
+			notes[note].stop()
 
 # preset mixer initialization arguments: frequency (44.1K), size
 # (16 bits signed), channels (mono), and buffer size (1KB)
 # then, initialize the pygame library
 pygame.mixer.pre_init(MIXER_FREQ, MIXER_SIZE, MIXER_CHANS,\
-    MIXER_BUFF)
+	MIXER_BUFF)
 pygame.init()
 
 # use the Broadcom pin mode
@@ -120,7 +120,7 @@ GPIO.setup(blue, GPIO.OUT)
 
 # create the actual notes
 for n in range(len(freqs)):
-    notes.append(Note(freqs[n], 1))
+	notes.append(Note(freqs[n], 1))
 
 # begin in a non-recording state and initialize the song
 recording = False
@@ -133,18 +133,18 @@ print "Press Ctrl+C to exit..."
 # detect when Ctrl+C is pressed so that we can reset the GPIO
 # pins
 try:
-    while (True):
-        # start a timer
-        start = time()
-        # play a note when pressed...until released (also
-        # detect play/record)
-        key = wait_for_note_start()
-        # note the duration of the silence
-        duration = time() - start
-        # if recording, append the duration of the silence
-        if (recording):
-            song.append(["SILENCE", duration])
-        # if the record button was pressed
+	while (True):
+		# start a timer
+		start = time()
+		# play a note when pressed...until released (also
+		# detect play/record)
+		key = wait_for_note_start()
+		# note the duration of the silence
+		duration = time() - start
+		# if recording, append the duration of the silence
+		if (recording):
+			song.append(["SILENCE", duration])
+		# if the record button was pressed
 		if (key == "record"):
 			# if not previously recording, reset the song
 			if (not recording):
